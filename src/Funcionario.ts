1a835baf4e7f74480519b9b03057d8cdaf8ee30d
joneilson <joneilson84@gmail.com>
2023-01-10 10:04:46 -0300

    add new class cargo

--- src/Funcionario.ts
+++ src/Funcionario.ts
@@ -1,7 +1,12 @@
+import { Cargo } from "./Cargo";
 import { Pessoa } from "./Pessoa";
 
 export class Funcionario extends Pessoa {
     constructor( cpf: string, nome: string, telefone: string, private salario: number) {
         super(cpf, nome, telefone)
+    }
+
+    adicionarCargo(...cargos: Cargo[]) {
+        const novoCargo = new Cargo();
     }
 }
