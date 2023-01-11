1f16189f624f055d2e261a43fd1080a7861f6aef
joneilson <joneilson84@gmail.com>
2023-01-11 19:11:47 -0300

    Endereco and cliente completed

--- src/Endereco.ts
+++ src/Endereco.ts
@@ -1,59 +1,11 @@
 export class Endereco {
 
-    private _cep: string = '';
-    private _logradouro: string = '';
-    private _numero: string = '';
-    private _complemento: string = '';
-    private _cidade: string = '';
-    private _uf: string = '';
-
-    constructor() {}
-
-    get cep(): string {
-        return this._cep
-    }
-
-    get logradouro(): string {
-        return this._logradouro;
-    }
-
-    get numero(): string {
-        return this._numero;
-    }
-
-    get complemento(): string {
-        return this._complemento;
-    }
-
-    get cidade(): string {
-        return this._cidade;
-    }
-
-    get uf(): string {
-        return this._uf;
-    }
-
-    set cep(valor: string) {
-        this._cep = valor;
-    }
-
-    set logradouro(valor: string) {
-        this._logradouro = valor;
-    }
-
-    set numero(valor: string) {
-        this._numero = valor;
-    }
-
-    set complemento(valor: string) {
-        this._complemento = valor;
-    }
-
-    set cidade(valor: string) {
-        this.cidade = valor;
-    }
-
-    set uf(valor: string) {
-        this._uf = valor;
-    }
+    constructor(
+        private _cep: string,
+        private _logradouro: string,
+        private _numero: string,
+        private _complemento: string,
+        private _cidade: string,
+        private _uf: string
+    ) {}
 }
